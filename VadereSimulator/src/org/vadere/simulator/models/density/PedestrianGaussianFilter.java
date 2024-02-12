5ef6822f1592b20f0e4e315231cf1c479879fb2e
Nicklas Bocksberger <nb@principiamentis.com>
2024-02-12 17:55:27 +0100

    Loewen implementation

--- VadereSimulator/src/org/vadere/simulator/models/density/PedestrianGaussianFilter.java
+++ VadereSimulator/src/org/vadere/simulator/models/density/PedestrianGaussianFilter.java
@@ -6,7 +6,14 @@ import org.vadere.state.scenario.Pedestrian;
 import org.vadere.util.geometry.shapes.VPoint;
 import org.vadere.util.logging.Logger;
 
+import edu.hm.teamLoewen.ServiceNotRunningException;
+import edu.hm.teamLoewen.PredictionService;
+import edu.hm.teamLoewen.PotField;
+
+
+import java.io.IOException;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.function.Predicate;
 
 public class PedestrianGaussianFilter<E extends Pedestrian> implements IGaussianFilter {
@@ -15,11 +22,52 @@ public class PedestrianGaussianFilter<E extends Pedestrian> implements IGaussian
     private final Collection<E> pedestrians;
     private final Predicate<E> pedestrianPredicate;
     private final IPedestrianLoadingStrategy pedestrianLoadingStrategy;
+    private final PredictionService predictionService;
+    private int step;
     private static Logger logger = Logger.getLogger(PedestrianGaussianFilter.class);
+    private static LinkedList<PotField.Field> fields = new LinkedList<PotField.Field>();
 
     public PedestrianGaussianFilter(final Collection<E> pedestrians, final IGaussianFilter filter,
                                     final IPedestrianLoadingStrategy pedestrianLoadingStrategy) {
         this(pedestrians, filter, pedestrianLoadingStrategy, p -> true);
+    }
+
+    private static PotField.Field filterToField(IGaussianFilter filter) {
+        int height = filter.getMatrixHeight();
+        int width = filter.getMatrixWidth();
+        PotField.Field.Builder fieldBuilder = PotField.Field.newBuilder();
+        for (int x=0; x < width; x++) {
+            for (int y=0; y < height; y++) {
+                double val = filter.getFilteredValue(x, y);
+                PotField.Coordinate.Builder coord = PotField.Coordinate.newBuilder();
+                coord.setX(x).setY(y).setValue(val);
+                fieldBuilder.addCoordinates(coord.build());
+            }
+        }
+        return fieldBuilder.build();
+    }
+
+    private boolean usePrediction() {
+        if (step < 30) return false; // do not use predictions for first 30 seconds
+        return step % 30 > 5;  // every 30 seconds, predict 5 steps
+    }
+
+    private void saveFilter(IGaussianFilter filter) {
+        PotField.Field field = filterToField(filter);
+        fields.add(field);
+    }
+
+    private void makePrediction() {
+        PotField.FieldSequence.Builder sequenceBuilder = PotField.FieldSequence.newBuilder();
+        int fieldLength = fields.size();
+        for (int idx = fieldLength - 10; idx < fieldLength; idx++) {
+            sequenceBuilder.addFields(fields.get(idx));
+        }
+        PotField.FieldSequence fieldSequence = sequenceBuilder.build();
+        predictionService.fit(fieldSequence);
+
+        PotField.FieldSequence predictions = predictionService.predict(fieldSequence, 30);
+        fields.addAll(predictions.getFieldsList());
     }
 
     public PedestrianGaussianFilter(final Collection<E> pedestrians, final IGaussianFilter filter,
@@ -28,32 +76,34 @@ public class PedestrianGaussianFilter<E extends Pedestrian> implements IGaussian
         this.pedestrians = pedestrians;
         this.pedestrianPredicate = pedestrianPredicate;
         this.pedestrianLoadingStrategy = pedestrianLoadingStrategy;
+        this.predictionService = new PredictionService();
+        this.step = 0;
+        try {
+            predictionService.startService();
+        } catch (ServiceNotRunningException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public double getFilteredValue(int x, int y) {
 
-        double value = filter.getFilteredValue(x, y);
-		/*
-		 * if(value > 0) {
-		 * logger.info("pedVal: " + value);
-		 * }
-		 */
-        // return value < 1.0f ? 0 : value;
-        return value;
+        PotField.Field field = fields.getLast();
+        // not very efficient but it avoids converting to another data type and therefore
+        // hopefully makes the whole code better understandable
+        for (PotField.Coordinate coord : field.getCoordinatesList()) {
+            if (coord.getX() == x && coord.getY() == y) return coord.getValue();
+        }
+        return 0.0; // unknown coordinate, shouldn't happen, for prod use you'd raise an exception
     }
 
     @Override
     public double getFilteredValue(double x, double y) {
-
-        double value = filter.getFilteredValue(x, y);
-		/*
-		 * if(value > 0) {
-		 * logger.info("pedVal: " + value);
-		 * }
-		 */
-        // return value < 1.0f ? 0 : value;
-        return value;
+        return getFilteredValue(filter.toXIndex(x), filter.toYIndex(y));
     }
 
     @Override
@@ -68,8 +118,14 @@ public class PedestrianGaussianFilter<E extends Pedestrian> implements IGaussian
 
     @Override
     public void filterImage() {
+        step++;
+        if (usePrediction()) {
+            if (step < fields.size()) return;
+            makePrediction();
+        }
         setValues();
         filter.filterImage();
+        saveFilter(filter);
     }
 
     @Override
@@ -130,6 +186,11 @@ public class PedestrianGaussianFilter<E extends Pedestrian> implements IGaussian
 	@Override
     public void destroy() {
         this.filter.destroy();
+        try {
+            predictionService.stopService();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     private void setValue(@NotNull final E pedestrian) {
